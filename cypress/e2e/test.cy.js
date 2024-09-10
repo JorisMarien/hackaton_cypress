1a7c85c7db5fa862e5ea610ee5f45ea3d4c23008
JorisMarien <m-joris@hotmail.be>
2024-09-10 14:39:32 +0200

    failonstatuscode

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,14 +3,14 @@ describe('hackaton test spec', () => {
     cy.visit('https://hackthefuture.bignited.be/')
     cy.wait(4000)
     cy.get('#action-button').click()
-    cy.wait(20000)
+    
+  })
+
+  it.only('information',()=>{
+    cy.visit('http://hackthefuture.bignited.be/information',{failOnStatusCode:false})
     cy.get('#name').type('naam')
     cy.get('#age').type('20')
     cy.get('#species').select('Reaper')
     cy.get('#planet').type('Rozemaan').type('{enter}')
-  })
-
-  it('information',()=>{
-   
   })
 })
